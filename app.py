5405840dcdf634031b24b02dd4310dbab1d988fe
Sverre <sverreglia@gmail.com>
2026-09-06 23:35:28 +0200

    La til kode for miljøvariabel

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route("/")
 def hello(): 
-	return "<h1>Hei fra Flask!</h1><p>Hei fra mitt virtuelle miljø 🎉</p>"
+	hemmelighet = os.getenv("SECRET_MESSAGE", "ingen hemmelighet satt")
+	return f"<h1>Hei fra Sverre!</h1><p>Jeg har gjort en endring! Hei fra mitt virtuelle miljø 🎉 {hemmelighet}</p>"
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
